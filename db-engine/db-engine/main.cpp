f5fd69aea12659bbcc7b88c3571592377fda0b60
mm <user@MohamedlefasAir.home>
2021-01-06 20:37:03 -0500

    test tpch 0

---
+++ db-engine/db-engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  db-engine
+//
+//  Created by User on 1/1/21.
+//  Copyright © 2021 User. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- src/common/runtime/Profile.cpp
+++
@@ -1,2 +0,0 @@
-
-bool writeHeader = true;
