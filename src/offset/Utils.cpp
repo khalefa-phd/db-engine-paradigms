d22a4c776b8949caa3f616d2767308bdf877cbe5
Tomás Felipe Llano Ríos <tllano11@gmail.com>
2021-02-02 11:21:08 -0500

    Fixing condition on possible match encounter

--- src/offset/Utils.cpp
+++ src/offset/Utils.cpp
@@ -19,11 +19,8 @@ inline __m256i _mm256_cmplt_epi32(__m256i a, __m256i b) {
 inline size_t offset::utils::avx2_count_matches(unsigned* firstBegin, unsigned* firstEnd,
                             unsigned* secondBegin, unsigned* secondEnd) {
    size_t counter = 0;
-   __m256i a_rep;
-   __m256i b;
-
-   a_rep = _mm256_set1_epi32(*firstBegin);
-   b = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(secondBegin));
+   __m256i a_rep = _mm256_set1_epi32(*firstBegin);
+   __m256i b = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(secondBegin));
 
    while (firstBegin < firstEnd && secondBegin < secondEnd) {
       const __m256i lt = _mm256_cmplt_epi32(b, a_rep);
@@ -36,7 +33,8 @@ inline size_t offset::utils::avx2_count_matches(unsigned* firstBegin, unsigned* 
          // there might be an element equal to the element pointed to by the first vector iterator, so
          // do a simple linear search, as there're only 8 elements to search in
          for (size_t i = 0; i < 8; i++) {
-            if (*(secondBegin + i) == *firstBegin) {
+            auto possibleMatch = secondBegin + i;
+            if ((possibleMatch < secondEnd) && (*possibleMatch == *firstBegin)) {
                counter++;
                break;
             }
